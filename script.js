ac0011307a8b18a9febe89943782a1feff198389
joaopedro <joaoribeiroc3po@gmail.com>
2023-12-07 21:08:37 -0300

    atualizacao

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function toggleMenu() {
     const navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('show');
+
+    const drawerLinks = document.querySelector('.drawer');
+    drawerLinks.classList.toggle('show');
 }
